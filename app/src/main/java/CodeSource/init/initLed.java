e16ad5eb98bdd1c9c8056fff12a90d15d2c5a9e3
seblan <sebastien.lanaspeze@gmail.com>
2016-07-06 11:25:10 +0200

    oublie d'une ed pour 'linitialisation

--- app/src/main/java/CodeSource/init/initLed.java
+++ app/src/main/java/CodeSource/init/initLed.java
@@ -10,7 +10,7 @@ public class initLed {
     public static ArrayList<Integer> initLED(){
         ArrayList<Integer> led = new ArrayList<Integer>();
 
-        for (int i =0; i < 104; i++){
+        for (int i =0; i < 105; i++){
             led.add(0);
         }
 
